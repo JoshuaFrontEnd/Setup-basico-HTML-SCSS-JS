73c148ffbe97765da7e2bda0d67bad65df878324
Joshua <hola@joshua.cl>
2020-02-12 18:34:52 -0300

    Se cambia completamente la estructura de archivos por una mas idonea para la entrega a produccion y se agrega media querie sort

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,33 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync').create();
-var sass = require('gulp-sass');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const postcss = require('gulp-postcss');
+const sortCSSmq = require('sort-css-media-queries');
+const browserSync = require('browser-sync').create();
 
 // Static Server + watching scss/html files
 gulp.task('serve', function () {
   browserSync.init({
-    server: './public'
+    server: './html'
   });
-
-  gulp.watch('scss/**/*.scss', ['sass']);
-  gulp.watch('public/*.html').on('change', browserSync.reload);
-  gulp.watch('public/js/*.js').on('change', browserSync.reload);
+  gulp.watch('html/assets/sass/**/*.scss', ['sass']);
+  gulp.watch('html/*.html').on('change', browserSync.reload);
+  gulp.watch('html/assets/js/*.js').on('change', browserSync.reload);
 });
 
 // Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
-  return gulp.src('scss/styles.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('public/css'))
+  var postCSSplugins = [
+    require('autoprefixer')({
+      browsers: ['last 2 versions', '> 2%']
+    }),
+    require('css-mqpacker')({
+      sort: sortCSSmq
+    })
+  ];
+  return gulp.src('html/assets/sass/*.scss')
+    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
+    .pipe(postcss(postCSSplugins))
+    .pipe(gulp.dest('html/assets/css'))
     .pipe(browserSync.stream());
 });
 
